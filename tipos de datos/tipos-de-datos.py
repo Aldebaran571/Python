32f544cd07d05bbdd93ad9ef33d4258f095be652
john <munra05@gmail.com>
2024-08-21 15:15:27 -0500

    tipos de datos

--- tipos de datos/tipos-de-datos.py
+++ tipos de datos/tipos-de-datos.py
@@ -1,2 +1,20 @@
 print("hola mundo")
 print(2+2)
+
+"string"
+'string'
+"""string"""
+
+"""los datos del usuario son:
+
+nombre: pepito
+apellido: perez
+edad: 12"""
+
+'''string
+los datos del usuario son:
+
+nombre: pepito
+apellido: perez
+edad: 12
+'''

96d61130d343305507a78d9f6ddce68976a49788
john <munra05@gmail.com>
2024-08-22 14:48:27 -0500

    ejercicio de comparacion

---
+++ operadores/op_comparacion.py
@@ -0,0 +1,13 @@
+#
+igual_que = 5==6
+
+diferente_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+print(igual_que, diferente_de, mayor_que, menor_que, mayor_o_igual, menor_o_igual)

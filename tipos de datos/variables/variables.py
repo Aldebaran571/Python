925e91190c81752eb7323f05e18fa93b153a5a33
john <munra05@gmail.com>
2024-08-22 12:34:49 -0500

    añadir comentarios al codigo

--- tipos de datos/variables/variables.py
+++ tipos de datos/variables/variables.py
@@ -1,3 +1,9 @@
-nombre = 5
-bienvenido = f"hola  {nombre}  como va la causa?"  
-print ("hola" not in bienvenido)
+#definiendo una variable
+nombre = "john"
+#concatenar con f-strings
+bienvenido = f"hola  {nombre}  como va la causa?" 
+#concatenar con +
+bienvenido = "hola" +nombre+ "como va todo?" 
+print ("hola" in bienvenido)#true
+print ("hola" not in bienvenido)#false
+#operadores de pertenencia el (in - not in) se usan como si fueran valores booleanos en las variables

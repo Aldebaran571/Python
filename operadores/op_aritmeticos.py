ec3bb9589b14e13c5c01438c64264d297c20fdd1
john <munra05@gmail.com>
2024-08-22 14:33:49 -0500

    se agregan type, suma, resta, multi, diuvision

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,20 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+#multiplicacion y division (* y /)
+multi = 12 * 5
+division  = 12 / 5 #la divisdion devuelve un dato tipo float
+
+#potenciacion exponente (**)
+exponente = 12 ** 5
+
+#division baja? (//)
+division_baja = 12 // 5 #devuelve entero redondea hacia abajo   
+
+#porcentaje o modulo (%)
+modulo = 12 % 5
+print(suma, resta, multi, division, exponente, division_baja, modulo)
+
+#type nos muestra que tipo de dato es, si es INT, Float, string...
+tipo_de_dato = type(5)
+print(tipo_de_dato)

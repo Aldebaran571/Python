2da3fbda3f70b31bc4acf3fe1e07d0dffcc4c37a
john <munra05@gmail.com>
2024-08-23 14:44:00 -0500

    prueba aritmetica

--- operadores/op_comparacion.py
+++ operadores/op_comparacion.py
@@ -11,3 +11,9 @@ mayor_o_igual = 5 >= 6
 
 menor_o_igual = 5 <= 6
 print(igual_que, diferente_de, mayor_que, menor_que, mayor_o_igual, menor_o_igual)
+
+a = 5
+b = 10
+c = 15
+resultado = a + b == c
+print(resultado)

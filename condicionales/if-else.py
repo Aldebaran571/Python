e784958f8ea483b9d4d4691fe0655b2c545e4e81
john <munra05@gmail.com>
2024-08-23 17:20:44 -0500

    EJERCICIOS DE PRACTIA

--- condicionales/if-else.py
+++ condicionales/if-else.py
@@ -1,3 +1,4 @@
+"""EJERCICIO 1
 edad = 19
 
 if edad <=18:
@@ -5,3 +6,25 @@ if edad <=18:
 else:
     edad>=18
     print("siga con confianza")
+EEJERCICIO 2
+    
+numero = 8
+if numero >0 & numero <= 5:
+    print("el numero es positivo ")
+else:
+    numero < 0
+    print("el numero es negativo")
+    
+    EJERCICIO 3
+    
+notas_aprobadas = 2
+
+if notas_aprobadas <=5 & notas_aprobadas >=4:
+    print("aprobado")
+elif notas_aprobadas <=2.9:
+    print("debe recuperar")
+else:
+    notas_aprobadas =3
+    print("pasó raspando")
+    
+    EJERCICIO 4"""

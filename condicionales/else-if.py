9ba0189a03105318131b7ea38660819654d7c911
john <munra05@gmail.com>
2024-08-23 15:18:28 -0500

    probando if anidaDOS

---
+++ condicionales/else-if.py
@@ -0,0 +1,23 @@
+ingreso_mensual = 100000
+gasto_mensual = 80000
+
+if ingreso_mensual >10000:
+    print("esta }bien en cualquier lado")
+    if ingreso_mensual - gasto_mensual < 0:
+        print("esta en deficit")
+    elif ingreso_mensual -gasto_mensual >3000:
+        print("vamos bien")
+    else:("esta gastando demasiado")    
+    
+    
+elif ingreso_mensual >1000:
+    print("esta bien en latam")
+    
+elif ingreso_mensual >100:
+    print("esta bien en col")
+    
+elif ingreso_mensual >10:
+    print("esta bien  jodido")    
+
+else:
+    print("estamos en la olla papá")

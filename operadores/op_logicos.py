4fa32e0b7b5fb0628f49ed129fb07f90f0f415ec
john <munra05@gmail.com>
2024-08-23 16:45:43 -0500

    ejemplos basicos de como  funciona los operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,17 @@
+#AND
+resultado = True & True #devuelve true
+resultado2 = False & True #devuelve false
+resultado3 = True & False #devuelve false
+resultado4 = False & False #devuelve false
+
+#OR
+resultado5 = True | True #devuelve true
+resultado6 = False | True #devuelve true
+resultado7 = True | False #devuelve true
+resultado8 = False | False #devuelve false
+
+#NOT 
+resultado9 = not True #devuelve false
+resultado10 = not False #devuelve true
+
+print(resultado7)

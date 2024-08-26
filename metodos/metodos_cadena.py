ca895e8bb7449accf47eae97464ef358ef15f7c9
john <munra05@gmail.com>
2024-08-26 14:39:14 -0500

    nuevos metodos mas implementacion

--- metodos/metodos_cadena.py
+++ metodos/metodos_cadena.py
@@ -1,4 +1,4 @@
-cadena1 = "jOhN Nieto"
+cadena1 = "mereuetengue"
 cadena2 = "MODO BEstiA"
 #CONVIERTE A MAYUSCULAS
 mayus = cadena1.upper()#dato.nombredelmetodo()
@@ -13,4 +13,20 @@ primer_letra_mayus = cadena1.capitalize()
 #busca una cadena en otra cadena, si no hay coincidencias pone -1 , recordar que distingue mayusculas de minusculas
 busqueda_find = cadena1.find("e")
 
-print(busqueda_find)
+#busca una cadena dentro de otra cadena, si no hay coincidencias, arroja error de execpción
+busqueda_index = cadena1.index("e")
+
+
+#si es numerico devuelve True caso contrario devuelve False
+es_numerico = cadena1.isnumeric()
+
+
+#si es alfanumerico y no hay espacios ni otros caracteres especiales, determina si hay letras
+es_alfa = cadena1.isalpha()
+
+#busca una cadena en otra cadena, busca la cantidad de letras que hay en el texto de la variable
+contar_coincidencia = cadena1.count("e")
+
+#la funion len nos cuenta la cantidad de caracteres en una cadena
+contar_caracteres = len(cadena1)
+print(contar_caracteres)

6abaa86e1c4b1db9b03c97e63c82ec6b1ab60687
john <munra05@gmail.com>
2024-08-26 13:59:15 -0500

    implementacion nuevos metodos

--- metodos/metodos_cadena.py
+++ metodos/metodos_cadena.py
@@ -6,3 +6,11 @@ mayus = cadena1.upper()#dato.nombredelmetodo()
 #CONVIERTE A MINUSCULAS
 minus = cadena2.lower()
 print(mayus, minus)
+
+#cambia la primera letra a mayuscula
+primer_letra_mayus = cadena1.capitalize()
+
+#busca una cadena en otra cadena, si no hay coincidencias pone -1 , recordar que distingue mayusculas de minusculas
+busqueda_find = cadena1.find("e")
+
+print(busqueda_find)

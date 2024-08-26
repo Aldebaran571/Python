90198beccc23a47fd34aae14bd741712de46630c
john <munra05@gmail.com>
2024-08-26 13:50:31 -0500

    correcion de errores

--- metodos/metodos_cadena.py
+++ metodos/metodos_cadena.py
@@ -1,4 +1,8 @@
-cadena1 = "john nieto"
-cadena2 = "aprendiendo como una bestia"
-resultado = cadena1.upper()#dato.nombredelmetodo()
-print(resultado)
+cadena1 = "jOhN Nieto"
+cadena2 = "MODO BEstiA"
+#CONVIERTE A MAYUSCULAS
+mayus = cadena1.upper()#dato.nombredelmetodo()
+
+#CONVIERTE A MINUSCULAS
+minus = cadena2.lower()
+print(mayus, minus)

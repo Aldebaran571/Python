40e1a3310178eba240b544eceadc262c14f54e4e
john <munra05@gmail.com>
2024-08-27 13:51:36 -0500

    nuevas inclusiones en listas

--- metodos/metodos_listas.py
+++ metodos/metodos_listas.py
@@ -1,4 +1,16 @@
 #creando una lista con list
 lista = list(["hola", "john", "nieto", 37, 554, 10])
 
+#devuelve la cantidad dde elementos de la lista
+cantidad_elementos = len(lista)  
+
+#agregando elemento a la lista
+agregando_con_append = lista.append("jajajaja")
+
+#agregando con insert
+lista.insert(2, "jelou, perra ")
+
+#agregando varios elementos a la lista
+lista.extend([True, 5050])
+
 print(lista)

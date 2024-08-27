be497d840f2f11b4a87b798993825d522ab16865
john <munra05@gmail.com>
2024-08-27 14:23:30 -0500

    se corrigen errores

--- metodos/metodos_listas.py
+++ metodos/metodos_listas.py
@@ -1,14 +1,14 @@
 #creando una lista con list
-lista = list(["hola", "john", "nieto", 37, 554, 10])
+lista = list([ 37, 554, 10, False])
 
 #devuelve la cantidad dde elementos de la lista
 cantidad_elementos = len(lista)  
 
 #agregando elemento a la lista
-agregando_con_append = lista.append("jajajaja")
+agregando_con_append = lista.append(19854)
 
 #agregando con insert
-lista.insert(2, "jelou, perra ")
+lista.insert(2, 7)
 
 #agregando varios elementos a la lista
 lista.extend([True, 5050])
@@ -17,5 +17,18 @@ lista.extend([True, 5050])
 lista.pop(-1)
 
 #removiendo un elemento de la lista por su valor.
+#lista.remove("hola")
 
-print(lista)
+#eliminando todos los elementos de la lista
+#lista.clear()
+#ordenando la lista, si se usa con el parametro reverse=True los ordena al reves
+lista.sort()#(reverse=True)
+
+#invirtiendo los parametros de una lista
+lista.reverse()
+
+#verificando si un elemento se encuentra en la lista
+elemento_encontrado = lista.index(37)
+
+
+print(elemento_encontrado)

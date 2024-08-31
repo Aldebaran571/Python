0b67363ec1847f7e285914abee4ed628d234ddec
john <munra05@gmail.com>
2024-08-30 20:30:37 -0500

    se ingresa valores numericos

--- inputs/input_numeros.py
+++ inputs/input_numeros.py
@@ -1,7 +1,10 @@
 numero = input("dame un numero: ")
-#pide al susuario un numer y lo multiplica por 2
-resultado = int(numero)*2
+#pide al susuario un numero y lo multiplica por 2
+resultado = float(numero)
 
 #pide un numero al usuario en valores decimales
 resultado_flotante = float(numero)*2
 print(resultado_flotante)
+
+resultado_entero = int(numero)*2
+print(resultado_entero)

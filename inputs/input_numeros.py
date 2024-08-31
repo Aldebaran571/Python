5ee5436fe8866fbca71b1f48e9c0e5bb1c6ca542
john <munra05@gmail.com>
2024-08-30 20:12:05 -0500

    se usa el input para pedir un nombre

---
+++ inputs/input_numeros.py
@@ -0,0 +1,7 @@
+numero = input("dame un numero: ")
+#pide al susuario un numer y lo multiplica por 2
+resultado = int(numero)*2
+
+#pide un numero al usuario en valores decimales
+resultado_flotante = float(numero)*2
+print(resultado_flotante)

e9e22fe89283c883d03ca6ab11bf658b2152fefd
john <munra05@gmail.com>
2024-08-30 19:41:47 -0500

    agregando metodos

--- metodos/metodos_dict.py
+++ metodos/metodos_dict.py
@@ -3,5 +3,14 @@ diccionario = {#tambien sirven para iterar
     "apellido" : 'nieto',
     "edad" : 38
 }
-claves = diccionario.get("nombre")
+#nos devuelve un objeto dict_item 
+claves = diccionario.keys()
+
+#obteniendo un elemnto con get(si no encuentra nada el programa continua)
+valor_de_vergacion = diccionario.get("vergacion")
+print("que se dice colega?")
+
+#eliminando todos los datos del diccionario
+#diccionario.clear()
+diccionario.pop("nombre")
 print(claves)

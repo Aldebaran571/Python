54c3f7d9ce80634bd12ea844afe6b229522f3bb7
john <munra05@gmail.com>
2024-09-02 15:17:57 -0500

    creacion de archivos y ejemplos de conjuntos y tuplas

---
+++ variables 2.0/desempaquetado.py
@@ -0,0 +1,6 @@
+#creando datos
+datos_tupla = ("john", "nieto", 38)
+datos_lista = ["john", "nieto", 38]
+
+#desempaquetado 
+nombre, apellido, edad = datos_tupla
---
+++ variables 2.0/tuplas.py
@@ -0,0 +1,11 @@
+#creando tuplas con tuple
+#tupla = tuple(["john", "nieto"])
+
+#creando una tupla sin parentesis con multiples datos
+tupla = "john", "nieto"
+
+#creando una tupla sin parentesis con un solo dato
+
+tupla = "john",#se agrega la (coma)al final para que diferencie una coma de un string
+
+print(tupla)

749631d6df43476cca68f0fcd7503becd3f6eb3b
john <munra05@gmail.com>
2024-09-03 07:53:54 -0500

    explicacion brewve de diccionarios

--- variables 2.0/diccionarios.py
+++ variables 2.0/diccionarios.py
@@ -1,2 +1,13 @@
+#creando diccionarios con dict()
 diccionario= dict (nombre ="john", apellido= "nieto")
+
+#las listas no pueden ser claves y usamos frozenset para metrer conjuntos
+diccionario = {frozenset(["john", "nieto"]):"okeypelisgha" }
+
+#creando diccionario con fromkeys() con dos parametros cambiando el valor por defecto a  "no se"
+diccionario = dict.fromkeys([ "nombre", "apellido" ], "no se")
+
+#creando diccionario con fromkeys() valor por defecto none
+diccionario = dict.fromkeys(["nombre", "apellido"])
+
 print(diccionario)

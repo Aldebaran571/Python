a384d1f86f9c756b536029efeabbc22c83644119
john <munra05@gmail.com>
2024-09-03 10:05:00 -0500

    se crea el archivo while y se hacen ejercicios de explicacion

---
+++ bucles/bucle_while.py
@@ -0,0 +1,8 @@
+
+contador = 0
+
+#mientras la condicion se cumpla el ciclo se va a ejecutar(repitiendo una y otra vez)
+while contador < 10:
+    print(contador)
+    contador +=1
+print("el ciclo while llego a su fin")

637d32ee3555b26db8c93b85667c2345b37c7be8
john <munra05@gmail.com>
2024-09-03 09:59:43 -0500

    se practica haciendo un for en una sola linea

--- bucles/mas_iteracciones.py
+++ bucles/mas_iteracciones.py
@@ -1,6 +1,7 @@
-
+#creando las listas
 frutas = ["manzana", "banano", "pera", "ciruela", "coco", "uva"]
 cadena = "##  HOLA JOHN  ##"
+numeros = [2,4,8,16,32]
 #evitando comer una pera con la sentencia continue 
 for fruta in frutas:
     if fruta == 'pera':
@@ -18,3 +19,7 @@ else:
 #recorrer una cadena de texto
 for letra in cadena:
     print(letra)
+
+#for en una linea de codigo(duplicando numeros )
+numeros_duplicados = [x*2 for x in numeros]
+print(numeros_duplicados)

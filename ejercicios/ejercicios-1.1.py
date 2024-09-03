4186216d28549cbe1d6a94e52723b694c9308d5a
john <munra05@gmail.com>
2024-09-03 12:14:49 -0500

    se corrige codigo usando funcion round al archivo ejercicios, se deja en # todos los ejercicios para comprobart la diferencia en el codigo

--- ejercicios/ejercicios-1.1.py
+++ ejercicios/ejercicios-1.1.py
@@ -5,9 +5,9 @@ otros_cursos_promedio = 4
 curso_chimbo = 1.5
 
 #diferencia de duracion
-diferencia_con_min = 100 - curso_chimbo / otros_cursos_min *100
-diferencia_con_max = 100 - curso_chimbo  *1000 // otros_cursos_max /10
-diferencia_con_promedio = 100 - curso_chimbo / otros_cursos_promedio *100
+diferencia_con_min = round(100 - curso_chimbo / otros_cursos_min * 100, 2)
+diferencia_con_max = round(100 - curso_chimbo * 1000 // otros_cursos_max / 10, 2)#diferencia_con_max = 100 - curso_chimbo  *1000 // otros_cursos_max /10
+diferencia_con_promedio = round(100 - curso_chimbo / otros_cursos_promedio * 100, 2)#diferencia_con_promedio = 100 - curso_chimbo / otros_cursos_promedio *100
 
 print(f'este curso chimbo tarda un {diferencia_con_min}% menos que el mas rapido')
 print(f'este curso chimbo tarda un {diferencia_con_max}% menos que el mas lento')

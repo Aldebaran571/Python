f68295f80859de9916db659a18614b0bd733caa6
john <munra05@gmail.com>
2024-09-03 12:25:39 -0500

    se explican un poco mas las funciones sum y all

--- funciones/build-in.py
+++ funciones/build-in.py
@@ -11,3 +11,15 @@ print(numero_mas_bajo)
 #redondeando un numero 
 numero = round(12.8614546, 2)# se coloca la coma segun cuantos numeros decimales se requiera
 print(numero)
+
+#retorna False ->0, vacio , False, none \ True -> distinto a 0, True, cadena
+resultado_bool = bool(1)
+print(resultado_bool)
+
+#retorna True solo si todos los elementos son verdaderos, en caso de tener un valor nulo regresa False
+resultado_all =all([12,25, False, "uyrdfgbyd"])
+print(resultado_all)
+
+#suma todos los valores de un itinerable
+suma_total = sum(numeros)
+print(suma_total)

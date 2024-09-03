bd5f438284aa0a62be9070c5462c25b51590817d
john <munra05@gmail.com>
2024-09-03 11:46:55 -0500

    listar funciones y mejorar codigo de ejercicios

---
+++ funciones/build-in.py
@@ -0,0 +1,13 @@
+numeros = [4,2,8,75,38]
+
+#encontrar el numero mayor de una lista
+numero_mas_alto = max(numeros)
+print(numero_mas_alto)
+
+#encontrar el numero mas bajo de la lista
+numero_mas_bajo = min(numeros)
+print(numero_mas_bajo)
+
+#redondeando un numero 
+numero = round(12.8614546, 2)# se coloca la coma segun cuantos numeros decimales se requiera
+print(numero)

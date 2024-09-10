42f16d2fbf928a9ac7db2817a9d6b184deb69cac
john <munra05@gmail.com>
2024-09-10 14:18:58 -0500

    se extraen caracteres del texto, se lee texto y se cierra texto

---
+++ archivos/leer_txt.py
@@ -0,0 +1,16 @@
+archivo = open("archivos\\texto_prueba.txt", encoding="UTF-8")
+
+#leer el archivo completo
+#archivo =archivo.read()
+
+#leer una sola linea
+linea =archivo.readline()
+
+#leer linea por linea
+#lineas= archivo.readline()
+
+#cerrar archivo
+archivo.close()
+
+
+print(linea)

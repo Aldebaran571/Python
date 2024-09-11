50e9bb137f2544a8f24f4d63eb4ca7945325f50d
john <munra05@gmail.com>
2024-09-11 11:52:01 -0500

    usando instrucciones para consulttar datos de los archivos

--- archivos/leer_csv_con_pandas.py
+++ archivos/leer_csv_con_pandas.py
@@ -1,3 +1,46 @@
 import pandas as pd
-archivo= pd.read_csv("archivos\\prueba2.csv")
-print(archivo)
+
+#usando la funcion read_csv para leer el archivo  CSV
+df= pd.read_csv("archivos\\prueba2.csv")
+df2 = pd.read_csv("archivos\\prueba2.csv")
+
+#obteniendo los dats de la columna nombre
+nombres =df["nombre"]
+
+#ordenando el dataframe por la edad
+ascendente = df.sort_values("edad")
+
+#ordenando de forma descendente
+df_descendente = df.sort_values("edad", ascending=False)
+
+
+#concatenando los dos dataframes
+df_concatenado = pd.concat([df, df2])
+
+
+#accediendo a la primera fila con head()
+primera_fila = df.head(2)
+
+
+#accediendo a la ultima fila con tail()
+ultima_fila =df.tail(1)
+
+#accedendo a la cantidad de filas y de columnas con shape
+filas_columnas_totales = df.shape
+
+#obteniendo las estadoisticas  del dataframe con describe
+
+df_info = df.describe()
+
+#accediendo  a un elemento especifico del dataframe con loc. accediendo a la edad
+elemento_especifico_loc = df.loc[2,"edad"]
+
+#accediendo a elemeto especifico del dataframe con iloc, accediendo a la edad
+elemento_especifico_iloc= df.iloc[2,2]
+
+#accediendo a la fila  con edad mayor de 30
+mayor_30 = df.loc[df["edad"]<30,:]
+
+
+print(mayor_30)
+

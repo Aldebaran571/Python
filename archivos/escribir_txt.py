e1164edeb0086c6b6ebbdd492f86b5c7f4b4b425
john <munra05@gmail.com>
2024-09-11 10:12:54 -0500

    ejercicios practicos con writlines

--- archivos/escribir_txt.py
+++ archivos/escribir_txt.py
@@ -1,3 +1,6 @@
-with open('archivos\\texto_prueba.txt',w, encoding="utf-8") as archivo:
+with open('archivos\\texto_prueba.txt','w', encoding="utf-8") as archivo:
     #sobreescribiendo el archivo
-    archivo.write("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    #archivo.write("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    archivo.writelines(["hola compa como va la causa\n", "en el nombre de dios un trabajo por favor"])
+    #agregando dos lineas con el comando writelines
+    archivo.writelines(["hola en ek bisqyue de ka cghube\n ka chinbeta  se perdio\n", "en el nombre de dios"])

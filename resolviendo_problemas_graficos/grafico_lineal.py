e8d826bcfe569b3fd9264dd65f4bd5c8d0bd39c6
john <munra05@gmail.com>
2024-09-11 13:48:09 -0500

    creando un nuevo archivo para ptra grafica

--- resolviendo_problemas_graficos/grafico_lineal.py
+++ resolviendo_problemas_graficos/grafico_lineal.py
@@ -2,4 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("")
+df = pd.read_csv("resolviendo_problemas_graficos\\gas.csv")
+#creando un grafico con los datos propuestos
+sns.lineplot(x="fecha", y="gases", data=df)
+
+#creanmdo un puntop en la grafica donde esta el indicador mas alto
+plt.plot("01-10", 33, "o")
+
+#mostrando el grafico
+plt.show()

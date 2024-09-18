2cb3c2c7376758016bed371c06918a1fb79397b0
john <munra05@gmail.com>
2024-09-18 15:04:06 -0500

    correcion de codigo para que introduzca valores enteros en vez de valores numericos

--- excepciones/excepciones.py
+++ excepciones/excepciones.py
@@ -1,13 +1,21 @@
+#creando funcion que sume numeros
 def sumar_dos ():
+    #iniciando un bucle
     while True:
-        a = input("numero 1: ")
-        b = input("numero 2: ")
+        #pidiendo numeros
+        a = input("digite primer numero: ")
+        b = input("digite segundo numero: ")
+        #intentandoo convertirlos  a enteros y sumarlos
         try:
             resultado =int (a) + int(b)
-            break
+        #si lanza una excepcion pedirle que ingresde los datos
         except:
             print("introduzca un numero , aguevado... ¬¬")
+        #si todo sale bien termina el bucle
+        else:
+            break    
     return resultado
-print(sumar_dos())
+#mostrando el resultado
+print(f'la suma de los numeros es: ', sumar_dos())
             
     
